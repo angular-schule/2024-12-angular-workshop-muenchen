342813fb7154a75d0504cff74adc7bf5d41cd581
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-12-12 14:46:28 +0100

    Option 3 (Signals, RxJs, kommt morgen)

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
+import { map } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
+
 
 @Component({
   selector: 'app-book-details',
@@ -11,13 +14,19 @@ export class BookDetailsComponent {
 
   route = inject(ActivatedRoute);
 
+  // Option 1
   // snapshot aktualisiert sich nicht 😞
-  //isbn = this.route.snapshot.paramMap.get('isbn');
+  // isbn = this.route.snapshot.paramMap.get('isbn');
 
-  isbn = signal('');
+  // Option 2
+  // isbn = signal('');
+  // constructor() {
+  //   this.route.paramMap.subscribe(paramMap => this.isbn.set(paramMap.get('isbn') || ''))
+  // }
 
-  constructor() {
-    this.route.paramMap.subscribe(paramMap => this.isbn.set(paramMap.get('isbn') || ''))
-  }
+  // Option 3 (hardcore RxJs + Signals)
+  isbn = toSignal(this.route.paramMap.pipe(
+    map(paramMap => paramMap.get('isbn') || '')
+  ), { initialValue: '' });
 
 }
