9834efc5e4f4954e5ff88d4f69c214e3eb1f74a7
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-12-12 14:35:35 +0100

    snapshot 🤨

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,9 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
