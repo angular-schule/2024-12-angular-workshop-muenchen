3f4f157de4b84132f4dcc2fe6159954f5e0f2790
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2024-12-13 13:46:44 +0100

    rxjs: mergeMap

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,32 +1,26 @@
 import { Component, inject, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { map } from 'rxjs';
+import { map, mergeMap } from 'rxjs';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { BookStoreService } from '../shared/book-store.service';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-book-details',
-  imports: [RouterLink],
+  imports: [RouterLink, AsyncPipe,JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss'
 })
 export class BookDetailsComponent {
 
   route = inject(ActivatedRoute);
+  bs = inject(BookStoreService);
 
-  // Option 1
-  // snapshot aktualisiert sich nicht 😞
-  // isbn = this.route.snapshot.paramMap.get('isbn');
+  book$ = this.route.paramMap.pipe(
 
-  // Option 2
-  // isbn = signal('');
-  // constructor() {
-  //   this.route.paramMap.subscribe(paramMap => this.isbn.set(paramMap.get('isbn') || ''))
-  // }
-
-  // Option 3 (hardcore RxJs + Signals)
-  isbn = toSignal(this.route.paramMap.pipe(
-    map(paramMap => paramMap.get('isbn') || '')
-  ), { initialValue: '' });
+    map(paramMap => paramMap.get('isbn') || ''),
+    mergeMap(isbn => this.bs.getSingleBook(isbn))
+  )
 
 }
--- rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
+++ rxjs-playground/src/app/exercises/unsubscribe/unsubscribe.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnDestroy } from '@angular/core';
-import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, tap } from 'rxjs';
+import { Subject, ReplaySubject, timer, Subscription, takeWhile, takeUntil, tap, shareReplay } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   templateUrl: './unsubscribe.component.html',
-  imports: [HistoryComponent, AsyncPipe]
+  imports: [AsyncPipe]
 })
 export class UnsubscribeComponent {
 
-  zahl = timer(0, 1000).pipe(
+  zahl = toSignal(timer(0, 1000).pipe(
     tap(console.log)
-  )
+  ))
 
 }
